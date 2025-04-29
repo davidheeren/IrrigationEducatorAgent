9b164e47a703e0e5fc263233eab2d39fc1bdb314
davidheeren <davidmheeren@gmail.com>
2025-04-29 11:16:09 -0500

    Json config files and parsing

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import openai
+import json
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,4 +10,11 @@ api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
 print("Hello World")
-print(api_key)
+print(f"API: {api_key}")
+
+with open("config/agent_config.json", "r") as acf:
+    agent_config = json.load(acf)
+
+# unknown is the default string if the json object does not contain the key
+agent_name = agent_config.get("name", "unknown")
+print(f"agent name: {agent_name}")
