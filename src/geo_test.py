773d274765b484ca25f03769a474a80321558f81
davidheeren <davidmheeren@gmail.com>
2025-05-03 16:44:39 -0500

    geo_test

---
+++ src/geo_test.py
@@ -0,0 +1,7 @@
+import geocoder
+
+g = geocoder.ip("me")
+
+print(f"Location: {g.city}, {g.state}, {g.country}")
+print(f"Latitude: {g.latlng[0] if g.latlng else 'Unknown'}")
+print(f"Longitude: {g.latlng[1] if g.latlng else 'Unknown'}")
