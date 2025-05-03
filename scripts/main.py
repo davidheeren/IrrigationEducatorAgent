fd612469192ee3cb3bc2b649d937f4d846503b2c
davidheeren <davidmheeren@gmail.com>
2025-05-03 15:04:42 -0500

    File and web search tools

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import json
-from agents import Agent, Runner
+from agents import Agent, FileSearchTool, Runner, WebSearchTool
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,10 +8,26 @@ load_dotenv()
 with open("config/agent_config.json", "r") as acf:
     agent_config = json.load(acf)
 
+tools = []
+file_search_config = agent_config["tools"]["file_search"]
+if file_search_config["enabled"]:
+    tools.append(
+        FileSearchTool(
+            file_search_config["vector_store_ids"],
+            file_search_config["max_num_results"],
+            file_search_config["include_search_results"],
+        )
+    )
+
+web_search_config = agent_config["tools"]["web_search"]
+if web_search_config["enabled"]:
+    tools.append(WebSearchTool())
+
 agent = Agent(
     name=agent_config["name"],
     instructions=agent_config["instructions"],
     model=agent_config["model"],
+    tools=tools,
 )
 
 seperator = "-" * 50
